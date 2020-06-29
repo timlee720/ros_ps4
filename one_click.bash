6774e333b77bdb53c03e274f45be1f2546200530
jacklin-chan <55632066+jacklin-chan@users.noreply.github.com>
2020-06-29 16:59:25 +0800

    Update one_click.bash

--- one_click.bash
+++ one_click.bash
@@ -1,1 +1,31 @@
 
+sleep 1
+
+sudo chmod 666 /dev/ttyUSB0
+sudo chmod 666 /dev/ttyUSB1
+
+sudo chmod 666 /dev/ttyACM0
+sudo chmod 666 /dev/ttyACM1
+sudo chmod 666 /dev/ttyACM2
+sudo chmod 666 /dev/ttyACM3
+
+
+
+#roscore &
+
+sleep 1
+#cd ~/robocon/catkin_ws
+source /opt/ros/melodic/setup.bash
+source /home/ubuntu/robocon/catkin_ws/devel/setup.bash
+nohup roscore &
+sleep 8
+
+nohup rosrun joy joy_node &
+sleep 5
+
+nohup rosrun ps4_bot mainControl.py &
+
+nohup rosrun rosserial_python serial_node.py /dev/ttyACM0 _baud:=57600 &
+sleep 3
+
+#nohup rosrun ps4_bot mainControl.py &
